f22aa98d80229afbb06b0f2ce57c3a5e31009a11
Rahma22M <148641023+Rahma22M@users.noreply.github.com>
2025-03-27 01:25:11 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-// backend/server.js
 const express = require('express');
 const cors = require('cors');
 const annonceRoutes = require('./routes/annonceENSroutes');
-const authRoutes = require('./routes/authRoutes'); // Ajout de la route d'authentification
+const authRoutes = require('./routes/authRoutes');
+const sondageRoutes = require('./routes/sondageRoutes'); // Ajout des routes pour les sondages
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/annonces', annonceRoutes);
-app.use('/api/auth', authRoutes); // Ajout de la route d'authentification
+app.use('/api/auth', authRoutes);
+app.use('/api/sondages', sondageRoutes); // Ajout de la route pour les sondages
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
